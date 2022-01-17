b0689815f6786f19fbc026a6e77e8f625f6f8dc9
Luiz Neto <luizgonzaga15@gmail.com>
2022-01-17 18:35:42 -0300

    Configurando os serviços

---
+++ src/Api.CrossCutting/DependencyInjection/ConfigureService.cs
@@ -0,0 +1,19 @@
+﻿using Api.Domain.Interfaces.Services.User;
+using Microsoft.Extensions.DependencyInjection;
+using service.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace crossCutting.DependencyInjection
+{
+    public class ConfigureService
+    {
+        public static void ConfigureDependenciesService(IServiceCollection serviceCollection)
+        {
+            serviceCollection.AddTransient<IUserService, UserService>();
+        }
+    }
+}
--- src/Api.Data/Context/ContextFactory.cs
+++ src/Api.Data/Context/ContextFactory.cs
@@ -20,3 +20,10 @@ namespace data.Context
         }
     }
 }
+
+
+
+
+
+
+
