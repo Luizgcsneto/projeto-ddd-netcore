a3a7f1fbda7c5e4e936a855dc6fdc25d933ba664
Luiz Neto <luizgonzaga15@gmail.com>
2022-01-30 00:26:25 -0300

    Início da criação dos Tokens

--- src/Api.Domain/Dtos/LoginDto.cs
+++ src/Api.Domain/Dtos/LoginDto.cs
@@ -9,9 +9,9 @@ namespace domain.Dtos
 {
     public class LoginDto
     {
-        [Required(ErrorMessage = "Email é um campo obrigatório para login")]
-        [EmailAddress(ErrorMessage = "Email em formato inválido")]
-        [StringLength(100,ErrorMessage = "Tamanha acima do permitido")]
+        [Required(ErrorMessage = "{0} é um campo obrigatório para login")]
+        [EmailAddress(ErrorMessage = "{0} em formato inválido")]
+        [StringLength(100,ErrorMessage = "Tamanho acima do permitido deve ter no máximo {1}")]
         public string Email { get; set; }
     }
 }
